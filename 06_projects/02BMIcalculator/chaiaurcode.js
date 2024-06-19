0b968cc448019611e791fb08ee2a56668b2c7ee1
shubhamhazard <shubham555bgp@gmail.com>
2024-06-19 22:41:13 +0530

    BMI calculator

---
+++ 06_projects/02BMIcalculator/chaiaurcode.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('form')
+
+// const height = parseInt(document.querySelector('#height').value)
+//the above usecase will give you empty
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = "Please give a valid height"
+    }
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = "Please give a valid weight"
+    }
+    else {
+        const bmi = (weight / ((height * height) / 10000).toFixed(2))
+        //show the result
+        results.innerHTML = `<span>${bmi}</span>`
+    }
+
+    // var bmi = parseInt(document.querySelector('#results').value)
+    // // console.log(bmi);
+    // const state = document.querySelector('#statement')
+    // if (bmi < 18.6) {
+    //     state.innerHTML = "You are under weight"
+    // }
+    // else if (bmi > 18.6 && bmi < 24.9) {
+    //     state.innerHTML = "You are normal"
+    // }
+    // else{
+    //     state.innerHTML = "You are over weight"
+    // }
+})
