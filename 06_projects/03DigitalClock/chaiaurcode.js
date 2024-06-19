0fbf129d19dd1017b7c6bc1f9b82814a29859022
shubhamhazard <shubham555bgp@gmail.com>
2024-06-19 22:40:47 +0530

    Digital clock

---
+++ 06_projects/03DigitalClock/chaiaurcode.js
@@ -0,0 +1,15 @@
+const clock = document.getElementById('clock')
+// document.querySelector('#clock')
+
+let date = new Date()
+console.log(date.toLocaleTimeString())
+
+
+// setInterval(() => {
+    
+// }, interval);
+setInterval(function(){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+},1000)
